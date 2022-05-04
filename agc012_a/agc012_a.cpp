e03f889b815b42c9a082e505707a7c05cbda2821
sinozakiy0829@gmail.com <sinozakiy0829@gmail.com>
2022-05-04 21:25:29 +0900

    pushing of agc012_a

--- agc012_a/agc012_a.cpp
+++ agc012_a/agc012_a.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 // A ???
-long long ans, N, i, a[300001];
+//long long ans, N, i, a[300001];
 
 int main(){
 
+// いろいろ宣言（出力はintでは足りなくなる可能性あり）
+long N, ans=0;
 cin >> N;
+vector<long> a(3*N+1);
 
-for (i=1; i<=N*3; i++) cin >> a[i];
+// 入力
+for (int i=1; i<N*3+1; i++) cin >> a.at(i);
 
-sort(a+1, a+N*3+1);
+// ソート
+sort(a.begin(), a.end(), greater<long>());
 
-for (i=N*3-1; i>=N+1; i-=2) ans+=a[i];
+// 入力要素数を昇順に３等分した２番目の和が答え
+for (int i=1; i<2*N; i+=2) ans+=a.at(i);
 
 cout << ans << endl;
 
